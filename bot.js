5c8ba9846e2f1fa09793f4716a15cab947551e35
teenc <skellycantsleep@gmail.com>
2026-03-17 21:56:05 +0400

    vanity update  detection

--- bot.js
+++ bot.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
 const Eris = require("eris");
-const bot = new Eris(Process.env.TOKEN, {
+const bot = new Eris(process.env.TOKEN, {
     intents: ["guilds"]
 });
 
+bot.on("ready", () => {
+    console.log("bot online");
+});
 
+bot.on("guildUpdate", (guild, oldGuild) => {
+    const old_vanity = oldGuild.vanityURL
+    const new_vanity = guild.vanityURL
+    console.log(old_vanity);
+    console.log(new_vanity);
+});
 
 bot.connect();
