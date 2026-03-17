91735c5664548df6d57e95f00a00272577115f29
teenc <skellycantsleep@gmail.com>
2026-03-18 00:44:42 +0400

    hiding server on vanity update + dm on join

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const Eris = require("eris");
 const bot = new Eris(process.env.TOKEN, {
-    intents: ["guilds"]
+    intents: ["guilds", "guildMembers", "directMessages", "messageContent"]
 });
 
 bot.on("ready", () => {
@@ -11,9 +11,41 @@ bot.on("ready", () => {
 bot.on("guildUpdate", (guild, oldGuild) => {
     const old_vanity = oldGuild.vanityURL
     const new_vanity = guild.vanityURL
-    const server_id = guild.id
-    console.log(old_vanity, server_id);
-    console.log(new_vanity, server_id);
+    const everyone_role = guild.id
+    const new_perms = everyone_role.permissions & ~1024
+
+    if (new_vanity !== old_vanity) {
+        bot.editRole(guild.id, everyone_role, { permissions: new_perms}, "hiding server");
+    }
+
 });
+
+
+bot.on("guildMemberAdd", async (guild, member) => {
+    const user_id = member.user.id;
+    const guild_id = guild.id
+    const current_van = guild.vanityURL
+    const embed = {
+        description: `> **Vanity: [/${current_van}](https://discord.gg/${current_van})** \n> **Telegram: https://t.me/blahblahblah**`,
+        color: 0x101010
+    }
+    const embed2 = {
+        image: {
+        url: "https://i.pinimg.com/originals/c8/38/50/c838509cd460815c2f3ace89487e3fdf.gif"
+        },
+        color: 0x101010
+    }
+
+    try {
+        const dm = await bot.getDMChannel(user_id) 
+        await dm.createMessage({ embeds: [embed, embed2] })
+
+        await bot.kickGuildMember(guild_id, user_id, 'not allowed.')
+    } catch (error){
+        console.error(`${user_id} new member has joined ${guild_id}:`, error);
+    }
+});
+
+
 
 bot.connect();
